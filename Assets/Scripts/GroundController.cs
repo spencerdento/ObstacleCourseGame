4f04d9f69a59316ccc3a559f63900f650c343e4f
spencerdento <spencerdent02@gmail.com>
2020-08-23 21:03:45 -0500

    Movement and Camera

---
+++ Assets/Scripts/GroundController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundController : MonoBehaviour {
+
+    public float smooth;
+    private float frontRotation;
+    private float sideRotation;
+
+
+    // Start is called before the first frame update
+    void Start() {
+        smooth = 5.0f;
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate() {
+        frontRotation = Input.GetAxis("Vertical") * 35.0f;
+        sideRotation = Input.GetAxis("Horizontal") * -35.0f;
+
+        Quaternion target = Quaternion.Euler(frontRotation, 0, sideRotation);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+
+
+    }
+}
